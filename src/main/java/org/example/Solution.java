512efdc7a18ef7781c4bb34e486e5ca51a228dcf
alek <wait82985@gmail.com>
2023-06-06 20:59:27 +0600

    problem solved using bottob up dp approach. dp data structure used: list of maps

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.*;
+
 public class Solution {
     public int longestArithSeqLength(int[] nums) {
-        //TODO
-        return 0;
+        List<HashMap<Integer, Integer>> dp = new ArrayList<>();
+        int max = 2;
+        for (int i = 0; i < nums.length; i++) {
+            dp.add(new HashMap<>());
+            for (int j = 0; j < i; j++) {
+                int difference = nums[i] - nums[j];
+                int count = 2;
+
+                if (dp.get(j).containsKey(difference)) {
+                    count = Math.max(dp.get(j).get(difference), count) + 1;
+                    max = Math.max(max, count);
+                }
+                dp.get(i).put(difference, count);
+            }
+        }
+        return max;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -23,4 +23,11 @@ public class SolutionTest {
         int output = 4;
         Assert.assertEquals(output, new Solution().longestArithSeqLength(nums));
     }
+
+    @Test
+    public void longestArithSeqLengthTest4() {
+        int[] nums = {48,37,51,45,48,48,22,7,30,55,3,54,57,53,13,39,27,8,49,19,47,1,6,42,20,13,13,42,49,46,12,30,41,30,59,51,11,54,35,17,7,28,43,29,22,37,48,11,5,2,49,55,10,3,47,56,4,32,24,59,46,12,17,43,11,56,33,27,25,49,41,13,31,41,28,22,48,45,36,48,28,33,10,26,42,58,56,21,58,54,7,22,4,54,45,56,58,47,13,27};
+        int output = 6;
+        Assert.assertEquals(output, new Solution().longestArithSeqLength(nums));
+    }
 }
